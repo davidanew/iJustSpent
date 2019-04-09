eec367f7889a9abbe8eb98dc78e73912862817b1
davidanew <davidanew@googlemail.com>
2019-04-09 13:16:27 +0100

    Initial Commit

---
+++ iJustSpent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iJustSpent
+//
+//  Created by David New on 09/04/2019.
+//  Copyright © 2019 David New. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
