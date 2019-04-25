413ace15b253bc4693fa4d36a2b9b31dbbceba0e
davidanew <davidanew@googlemail.com>
2019-04-25 16:31:55 +0100

    Removed unused comments and blank lines

--- iJustSpent/InputView.swift
+++ iJustSpent/InputView.swift
@@ -11,28 +11,16 @@ class InputView: UIViewController {
     @IBOutlet weak var spentLabel: UILabel!
     //Collection view showing input spend options
     @IBOutlet weak var inputCollectionView: UICollectionView!
-    
+    //
     let controller = InputController()
-    
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //
         controller.spentLabelTextOutput.bind(to: spentLabel.rx.text).disposed(by: disposeBag)
-        
-        //controller.collectionViewInput.bind(to: self.inputCollectionView.rx.items(cellIdentifier: "cell" , cellType: InputCell.self)) { row, data, cell in cell.label.text = data }.disposed(by: disposeBag)
+        //
         controller.collectionViewArrayOutput.bind(to: self.inputCollectionView.rx.items(cellIdentifier: "cell" , cellType: InputCell.self), curriedArgument:  { row, data, cell in cell.label.text = data }).disposed(by: disposeBag)
-        
-        
-        //Observable.just(["1"]).bind(to: self.inputCollectionView.rx.items(cellIdentifier: "cell" , cellType: InputCell.self), curriedArgument:  { row, data, cell in cell.label.text = data }).disposed(by: disposeBag)
-        
-        
-        
-        
-        
-        
-        //Actions for if collection view cell is selected
+        //
         inputCollectionView.rx.itemSelected.subscribe(onNext: { (indexPath) in
             let cell = self.inputCollectionView.cellForItem(at: indexPath)
             //Flash cell to show selection
@@ -41,27 +29,11 @@ class InputView: UIViewController {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 cell?.backgroundColor = UIColor.red
             }
-            // Retieve the spend value associated with this cell
-            //let thisSpend = self.spendValues[indexPath.item]
-            // Add this spending. Result will propogate back through observables
-            //self.dataStore.addspend(thisSpend: thisSpend)
         }).disposed(by: disposeBag)
-        
-        
+        //
         inputCollectionView.rx.itemSelected.map{indexPath -> Int in return indexPath.item}.bind(to: controller.spentLabelSelectedInput).disposed(by: disposeBag)
-        
-        
-        
-        //inputCollectionView.rx.itemSelected.map{(indexpath) -> UIColor in return UIColor.green}.bind(to: self.inputCollectionView.rx.item, curriedArgument: <#T##R1#>)
-        //controller.collectionViewArrayOutput.subscribe(onNext: {print($0)})
-        
-        //Observable.just(UIColor.green).bind(to: self.inputCollectionView.rx.)
-        
+        //
         controller.doBindings()
-        
-
-      
-        
     }
 }
 
