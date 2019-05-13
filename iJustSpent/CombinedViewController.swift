63aa910da96845fead745636da19609867db03b7
davidanew <davidanew@googlemail.com>
2019-05-13 14:52:09 +0100

    Tableview highlighting done

--- iJustSpent/CombinedViewController.swift
+++ iJustSpent/CombinedViewController.swift
@@ -28,21 +28,31 @@ class CombinedViewController: UIViewController {
     //let controller = HistoryController()
     let spendStore = SpendStore()
     
+    /*
+    class EntryPickers {
+        static let picker1 : inout UISegmentedControl
+        
+    }
+ */
+ 
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+        let grayColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8)
+        let yellowColor = UIColor(red: 1, green: 0.7, blue: 0, alpha: 1)
         segControl.backgroundColor = .clear
-        segControl.tintColor = UIColor(red: 1, green: 0.7, blue: 0, alpha: 1)
-        let botButtonBackgroundColor = UIColor(red: 1, green: 0.7, blue: 0, alpha: 1)
-        botButton.backgroundColor = botButtonBackgroundColor
+        segControl.tintColor = yellowColor
+        botButton.backgroundColor = yellowColor
         botButton.layer.cornerRadius = 5
         botButton.setTitleColor(UIColor.black, for:UIControlState.normal)
         
         setupPickerViews()
         
-        //TODO: also map tap to clear
-        //TODO: map tap to button animation
         //TODO: map tap to history view entry
-        //TODO: dont add entry if it is zero
+        
+        
         
         botButton.rx.tap.map { [weak self] _ -> SpendDateAndValue in
             let unitsCombined = SpendIntType(self?.entryPicker2.selectedRow(inComponent: 0) ?? 0) * 100 +
@@ -51,26 +61,103 @@ class CombinedViewController: UIViewController {
             let subUnitsCombined = SpendIntType(self?.entryPicker6.selectedRow(inComponent: 0) ?? 0) * 10 +
                 SpendIntType(self?.entryPicker7.selectedRow(inComponent: 0) ?? 0)
             return SpendDateAndValue(date: Date(), units: unitsCombined, subUnits: subUnitsCombined)
-        }.bind(to: spendStore.newSpendInput).disposed(by: disposeBag)
+            }.filter{$0.units > 0 || $0.subUnits > 0}
+            .bind(to: spendStore.newSpendInput).disposed(by: disposeBag)
         
-        botButton.rx.tap.map{_ in return UIColor.red}.bind(to: botButton.rx.backgroundColor).disposed(by: disposeBag)
-        botButton.rx.tap.delay(0.2, scheduler: MainScheduler.instance).map{_ in return botButtonBackgroundColor}.bind(to: botButton.rx.backgroundColor).disposed(by: disposeBag)
+        botButton.rx.tap.map{_ in return grayColor}.bind(to: botButton.rx.backgroundColor).disposed(by: disposeBag)
+        botButton.rx.tap.delay(0.3, scheduler: MainScheduler.instance).map{_ in return yellowColor}.bind(to: botButton.rx.backgroundColor).disposed(by: disposeBag)
+        botButton.rx.tap.subscribe(onNext:{[weak self] _ in
+            self?.entryPicker2.selectRow(0, inComponent: 0, animated: true)
+            self?.entryPicker3.selectRow(0, inComponent: 0, animated: true)
+            self?.entryPicker4.selectRow(0, inComponent: 0, animated: true)
+            self?.entryPicker6.selectRow(0, inComponent: 0, animated: true)
+            self?.entryPicker7.selectRow(0, inComponent: 0, animated: true)
+        }).disposed(by: disposeBag)
         
-       
+        /*
+        botButton.rx.tap.map { _  in
+            return []
+            }.bind(to: historyTableView.rx.items(cellIdentifier: "CombinedCell", cellType: CombinedTableViewCell.self)) { row, model, cell in
+                cell.backgroundColor = UIColor.red
+            }.disposed(by: disposeBag)
+        */
+        
+        //TODO need to merge to get this to work maybe
+        
+        /*
+        
+        Observable.combineLatest(
+            spendStore.spendOutput,
+                                 botButton.rx.tap,
+                                 botButton.rx.tap.delay(0.3, scheduler: MainScheduler.instance))
+        {(value1 : () ,value2,value3) in
+            return nil
+            
+        }
+        */
+        
+        struct DayHistoryTableInput {
+            var date : String
+            var total : String
+        }
+        
+        enum TableviewUpdateEvent {
+            case spendOutput
+            case entryButtonDown
+            case entryButtonUp
+        }
+        
+        
+        //let x = TableviewUpdateEvent.spendOutput
+        
+        
+        let delme = Observable.combineLatest(spendStore.spendOutput,
+                                 spendStore.spendOutput.map{_ in return TableviewUpdateEvent.spendOutput},
+                                 botButton.rx.tap.map{_ in return TableviewUpdateEvent.entryButtonDown},
+                                 botButton.rx.tap.delay(0.3, scheduler: MainScheduler.instance).map{_ in return TableviewUpdateEvent.entryButtonUp},
+                                 
+                                 resultSelector: { (spendOutput : [SpendDateAndValue] ,  spendOutputEvent  , buttonDownEvent, buttonUpEvent) -> [SpendDateAndValue]  in
+                                    return spendOutput
+                                    
+        })
+        
+        _ = delme
+        
+        
+
+        
+        
+        
+   
+        
         spendStore.spendOutput.map { spendDateAndValueArray  in
+       //     print ("spendOutput")
             return GetTotalByDayForTableView.getTotalByDayForTableView(spendDateAndValueArray: spendDateAndValueArray )
-        }.bind(to: historyTableView.rx.items(cellIdentifier: "CombinedCell", cellType: CombinedTableViewCell.self)) { row, model, cell in
+        }.debug()
+            .bind(to: historyTableView.rx.items(cellIdentifier: "CombinedCell", cellType: CombinedTableViewCell.self)) { row, model, cell in
             cell.date.text = "\(model.date)"
             cell.spending.text = "\(model.total)"
             cell.layer.cornerRadius = 5
             cell.layer.masksToBounds = true
             //cell.backgroundColor = UIColor(red: 1, green: 0.7, blue: 0, alpha: 1)
-            cell.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.8)
+            cell.backgroundColor = grayColor
 
             cell.tintColor = UIColor.black
             cell.layer.borderWidth = 2
             cell.layer.borderColor = UIColor.black.cgColor
+            
+            
+            if row == 0 {
+                cell.backgroundColor = yellowColor
+             //   DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+             //       cell.backgroundColor = tableviewBackgroundColor
+             //   }
+            }
+            
             }.disposed(by: disposeBag)
+        
+        
+        
     }
     
     func setupPickerViews() {
@@ -123,7 +210,7 @@ class GetTotalByDayForTableView {
         let dayOfMonth = Calendar.current.component(.day, from: date)
         return ("\(weekDay) \(dayOfMonth) \(month)")
     }
-    
+    //todo needs to retunr today,yesterday etc
     static func getTotalByDayForTableView (spendDateAndValueArray : [SpendDateAndValue]) -> [DayHistoryTableInput] {
         struct UnitsAndSubunits {
             var units : SpendIntType
@@ -137,19 +224,19 @@ class GetTotalByDayForTableView {
                 return
             }
             let thisStartOfDay = Calendar.current.startOfDay(for: thisDate)
-            print(spendDateAndValue)
+       //     print(spendDateAndValue)
             if dayDictionary[thisStartOfDay] == nil {
                 dayDictionary[thisStartOfDay] = UnitsAndSubunits(units: spendDateAndValue.units, subUnits: spendDateAndValue.subUnits)
-                print("adding new entry")
+        //        print("adding new entry")
             }
             else {
-                print("amending entry")
+       //         print("amending entry")
                 let subUnitsSum = (dayDictionary[thisStartOfDay]?.subUnits ?? 999) + spendDateAndValue.subUnits
                 let subUnitsMod = subUnitsSum % 100
                 let subUnitsCarry = SpendIntType((subUnitsSum-subUnitsMod)/100)
                 let unitsSum = (dayDictionary[thisStartOfDay]?.units ?? 999) + spendDateAndValue.units + subUnitsCarry
-                print ("units sum \(unitsSum)")
-                print ("subUnitsMod \(subUnitsMod)")
+       //         print ("units sum \(unitsSum)")
+       //         print ("subUnitsMod \(subUnitsMod)")
                 dayDictionary[thisStartOfDay]?.units = unitsSum
                 dayDictionary[thisStartOfDay]?.subUnits = subUnitsMod
             }
