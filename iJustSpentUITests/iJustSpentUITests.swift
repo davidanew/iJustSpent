49a5ee131ad14697134423e523a85e68d31e1a51
davidanew <davidanew@googlemail.com>
2019-09-17 16:45:03 +0100

    UI test data added and deleted

--- iJustSpentUITests/iJustSpentUITests.swift
+++ iJustSpentUITests/iJustSpentUITests.swift
@@ -61,15 +61,66 @@ class iJustSpentUITests: XCTestCase {
     }
     
     
-    func testClearUserData(){
+    func addDataValue(inputTuple : (Int,Int,Int,Int,Int)){
+        let app = XCUIApplication()
+        let poundsHundredsPicker = app.pickers["poundsHundreds"]
+        let poundsTensPicker = app.pickers["poundsTens"]
+        let poundsUnitsPicker = app.pickers["poundsUnits"]
+        let penceTensPicker = app.pickers["penceTens"]
+        let penceUnitsPicker = app.pickers["penceUnits"]
+        poundsHundredsPicker.pickerWheels.element.adjust(toPickerWheelValue: String(inputTuple.0))
+        poundsTensPicker.pickerWheels.element.adjust(toPickerWheelValue: String(inputTuple.1))
+        poundsUnitsPicker.pickerWheels.element.adjust(toPickerWheelValue: String(inputTuple.2))
+        penceTensPicker.pickerWheels.element.adjust(toPickerWheelValue: String(inputTuple.3))
+        penceUnitsPicker.pickerWheels.element.adjust(toPickerWheelValue: String(inputTuple.4))
+        app.buttons["Add Spend"].tap()
+
+    }
+    
+    
+    func clearData(){
+        
+        
+        
+        
         //this will need to see if there is data in the table view, if so then delete
         //repeat above until empty
         let app = XCUIApplication()
+        
+        //let poundsHundredsPicker = app.pickers["poundsHundreds"]
+        //poundsHundredsPicker.pickerWheels.element.adjust(toPickerWheelValue: "1")
+        //app.buttons["Add Spend"].tap()
+        //poundsHundredsPicker.pickerWheels.element.adjust(toPickerWheelValue: "1")
+        //app.buttons["Add Spend"].tap()
+        
+        
+        //Thread.sleep(forTimeInterval: 1.0)
+        
         let myTable = app.tables.matching(identifier: "tableView")
+        //let myTable = app.table["table identifier"]
+        
         print(myTable.cells.count)
-        Thread.sleep(forTimeInterval: 5.0)
+
+        while myTable.cells.count > 0 {
+            app.buttons["Undo"].tap()
+            
+
+        }
+        print(myTable.cells.count)
+        
+        
+        
+        //Thread.sleep(forTimeInterval: 5.0)
 
         //let cell = myTable.cells.element(matching: .cell, identifier: "myCell_0")
+    }
+    
+    func testManualAddDataAndClear(){
+        addDataValue(inputTuple: (1,2,3,4,5))
+        Thread.sleep(forTimeInterval: 1.0)
+        clearData()
+        Thread.sleep(forTimeInterval: 5.0)
+
     }
     
     
